67ca3ebc49b2f2cd98ca5722a8de52ed6d29e9e5
Dongge021 <dongge021@gmail.com>
2026-06-19 14:28:47 +0800

    fix: add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join(__dirname, 'submissions.json');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
